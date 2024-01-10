b911b40a46630defaa281dc777ad6b0e50e0d6df
Dmitry Paramonov <dmitry@risingcloud.com>
2024-01-10 16:25:39 +0100

    init

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, watchEffect } from 'vue'
+
+const number = ref(null)
+const API_URL = import.meta.env.VITE_API_URL
+watchEffect(async () => {
+  console.log(import.meta.env.VITE_API_URL)
+  // this effect will run immediately and then
+  // re-run whenever currentBranch.value changes
+  const url = `${API_URL}/api/random`
+  let a = await fetch(url,{
+    mode: 'cors',
+  })
+
+  console.log(a)
+  number.value = await a.json()
+
+})
+</script>
+
+
+
+<template>
+  <label :for="number">{{ number }}</label>
+
+</template>
